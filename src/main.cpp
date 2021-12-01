8ebd7c7164322c441d721bdf9827b544b57e314f
Sille Van Landschoot <info@sillevl.be>
2021-12-01 17:23:22 +0100

    refactor hello world example

--- src/main.cpp
+++ src/main.cpp
@@ -12,23 +12,22 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
+#include "mbed.h"
 
 #include "tensorflow/lite/micro/all_ops_resolver.h"
-#include "tensorflow/lite/micro/micro_error_reporter.h"
 #include "tensorflow/lite/micro/micro_interpreter.h"
 #include "tensorflow/lite/schema/schema_generated.h"
-// #include "tensorflow/lite/version.h"
 
 #include "model.h"
 
-#include "mbed.h"
-#include "stdlib.h"
+using namespace tflite;
 
 
 // This constant represents the range of x values our model was trained on,
 // which is from 0 to (2 * Pi). We approximate Pi to avoid requiring additional
 // libraries.
-const float kXrange = 2.f * 3.14159265359f;
+const float Pi = 3.14159265359f;
+const float kXrange = 2.f * Pi;
 
 // This constant determines the number of inferences to perform across the range
 // of x values defined above. Since each inference takes time, the higher this
@@ -36,15 +35,13 @@ const float kXrange = 2.f * 3.14159265359f;
 // of this constant can be tuned so that one full cycle takes a desired amount
 // of time. Since different devices take different amounts of time to perform
 // inference, this value should be defined per-device.
-const int kInferencesPerCycle = 100;
+const int InferencesPerCycle = 100;
 
 PwmOut led(LED1);
 
-// Globals, used for compatibility with Arduino-style sketches.
-namespace {
-tflite::ErrorReporter* error_reporter = nullptr;
-const tflite::Model* model = nullptr;
-tflite::MicroInterpreter* interpreter = nullptr;
+
+const Model* model = nullptr;
+MicroInterpreter* interpreter = nullptr;
 TfLiteTensor* input = nullptr;
 TfLiteTensor* output = nullptr;
 int inference_count = 0;
@@ -52,45 +49,76 @@ int inference_count = 0;
 // Create an area of memory to use for input, output, and intermediate arrays.
 // Minimum arena size, at the time of writing. After allocating tensors
 // you can retrieve this value by invoking interpreter.arena_used_bytes().
-const int kModelArenaSize = 2468;
+const int ModelArenaSize = 2468;
 // Extra headroom for model + alignment + future interpreter changes.
-const int kExtraArenaSize = 560 + 16 + 100;
-const int kTensorArenaSize = kModelArenaSize + kExtraArenaSize;
-uint8_t tensor_arena[kTensorArenaSize];
-}  // namespace
+const int ExtraArenaSize = 560 + 16 + 100;
+const int TensorArenaSize = ModelArenaSize + ExtraArenaSize;
+uint8_t tensor_arena[TensorArenaSize];
 
-// The name of this function is important for Arduino compatibility.
-void setup() {
+
+void setLed(float value) {
+  led = abs(value);
+}
+
+// Output the results.
+// Log the current X and Y values
+
+void printValues(float x, float y) {
+  printf("x_value: % 2.3f, y_value: % 02.3f\n", x, y);
+}
+
+float infeer(float x) {
+  // Place our calculated x value in the model's input tensor
+  input->data.f[0] = x;
+
+  // Run inference, and report any error
+  TfLiteStatus invoke_status = interpreter->Invoke();
+  if (invoke_status != kTfLiteOk) {
+    printf("Invoke failed on x_val: %f\n", x);
+    return -1.0;
+  }
+
+  interpreter->Invoke();
+
+  // Read the predicted y value from the model's output tensor
+  return output->data.f[0];
+}
+
+
+
+
+int main(void) {
+  
   // Set up logging. Google style is to avoid globals or statics because of
   // lifetime uncertainty, but since this has a trivial destructor it's okay.
   // NOLINTNEXTLINE(runtime-global-variables)
-  static tflite::MicroErrorReporter micro_error_reporter;
-  error_reporter = &micro_error_reporter;
+  // static MicroErrorReporter micro_error_reporter;
+  // error_reporter = &micro_error_reporter;
 
   // Map the model into a usable data structure. This doesn't involve any
   // copying or parsing, it's a very lightweight operation.
-  model = tflite::GetModel(g_model);
+  model = GetModel(g_model);
   if (model->version() != TFLITE_SCHEMA_VERSION) {
-    TF_LITE_REPORT_ERROR(error_reporter,
-                         "Model provided is schema version %d not equal "
-                         "to supported version %d.",
-                         model->version(), TFLITE_SCHEMA_VERSION);
+
+    printf("Model provided is schema version %d not equal "
+          "to supported version %d.",
+          (int) model->version(), (int) TFLITE_SCHEMA_VERSION);
     return;
   }
 
   // This pulls in all the operation implementations we need.
   // NOLINTNEXTLINE(runtime-global-variables)
-  static tflite::AllOpsResolver resolver;
+  static AllOpsResolver resolver;
 
   // Build an interpreter to run the model with.
-  static tflite::MicroInterpreter static_interpreter(
-      model, resolver, tensor_arena, kTensorArenaSize, error_reporter);
+  static MicroInterpreter static_interpreter(
+      model, resolver, tensor_arena, TensorArenaSize, NULL);
   interpreter = &static_interpreter;
 
   // Allocate memory from the tensor_arena for the model's tensors.
   TfLiteStatus allocate_status = interpreter->AllocateTensors();
   if (allocate_status != kTfLiteOk) {
-    TF_LITE_REPORT_ERROR(error_reporter, "AllocateTensors() failed");
+    printf("AllocateTensors() failed");
     return;
   }
 
@@ -98,52 +126,23 @@ void setup() {
   input = interpreter->input(0);
   output = interpreter->output(0);
 
-  // Keep track of how many inferences we have performed.
-  inference_count = 0;
-}
-
-// The name of this function is important for Arduino compatibility.
-void loop() {
-  // Calculate an x value to feed into the model. We compare the current
-  // inference_count to the number of inferences per cycle to determine
-  // our position within the range of possible x values the model was
-  // trained on, and use this to calculate a value.
-  float position = static_cast<float>(inference_count) /
-                   static_cast<float>(kInferencesPerCycle);
-  float x_val = position * kXrange;
-
-  // Place our calculated x value in the model's input tensor
-  input->data.f[0] = x_val;
-
-  // Run inference, and report any error
-  TfLiteStatus invoke_status = interpreter->Invoke();
-  if (invoke_status != kTfLiteOk) {
-    TF_LITE_REPORT_ERROR(error_reporter, "Invoke failed on x_val: %f\n",
-                         static_cast<double>(x_val));
-    return;
-  }
-
-  // Read the predicted y value from the model's output tensor
-  float y_val = output->data.f[0];
-  led = abs(y_val);
-
-  // Output the results.
-  // Log the current X and Y values
-  TF_LITE_REPORT_ERROR(error_reporter, "x_value: %f, y_value: %f\n",
-                       static_cast<double>(x_val),
-                       static_cast<double>(y_val));
-
-  // Increment the inference_counter, and reset it if we have reached
-  // the total number per cycle
-  inference_count += 1;
-  if (inference_count >= kInferencesPerCycle) inference_count = 0;
-}
-
-
-int main(void) {
-  setup();
   while (true) {
-    loop();
+    // Calculate an x value to feed into the model. We compare the current
+    // inference_count to the number of inferences per cycle to determine
+    // our position within the range of possible x values the model was
+    // trained on, and use this to calculate a value.
+    float position = (float) inference_count / InferencesPerCycle;
+    float x_val = position * kXrange;
+
+    float y_val = infeer(x_val);
+
+    setLed(y_val);
+    printValues(x_val, y_val);
+
+    // Increment the inference_counter, and reset it if we have reached
+    // the total number per cycle
+    inference_count = (inference_count + 1) % InferencesPerCycle;
+    // if (inference_count >= InferencesPerCycle) inference_count = 0;
   }
 }
 
