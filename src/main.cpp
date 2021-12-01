1af8e769fc1f4f17d396bbf9bb1fcafc788898e7
Sille Van Landschoot <info@sillevl.be>
2021-12-01 17:51:01 +0100

    refactor tomake the example more easy to understand

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,3 @@
-/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
 #include "mbed.h"
 
 #include "tensorflow/lite/micro/all_ops_resolver.h"
@@ -23,110 +9,67 @@ limitations under the License.
 using namespace tflite;
 
 
-// This constant represents the range of x values our model was trained on,
-// which is from 0 to (2 * Pi). We approximate Pi to avoid requiring additional
-// libraries.
+// Application constants and variables
 const float Pi = 3.14159265359f;
-const float kXrange = 2.f * Pi;
-
-// This constant determines the number of inferences to perform across the range
-// of x values defined above. Since each inference takes time, the higher this
-// number, the more time it will take to run through the entire range. The value
-// of this constant can be tuned so that one full cycle takes a desired amount
-// of time. Since different devices take different amounts of time to perform
-// inference, this value should be defined per-device.
+const float xrange = 2.f * Pi;
 const int InferencesPerCycle = 100;
+int inference_count = 0;
 
-PwmOut led(LED1);
+// Structure that contains the generated tensorflow model
+const Model* model = GetModel(g_model);
 
-
-const Model* model = nullptr;
+// Interpreter, input and output structures
 MicroInterpreter* interpreter = nullptr;
 TfLiteTensor* input = nullptr;
 TfLiteTensor* output = nullptr;
-int inference_count = 0;
 
-// Create an area of memory to use for input, output, and intermediate arrays.
-// Minimum arena size, at the time of writing. After allocating tensors
-// you can retrieve this value by invoking interpreter.arena_used_bytes().
+// Allocate memory for the tensors
 const int ModelArenaSize = 2468;
-// Extra headroom for model + alignment + future interpreter changes.
 const int ExtraArenaSize = 560 + 16 + 100;
 const int TensorArenaSize = ModelArenaSize + ExtraArenaSize;
 uint8_t tensor_arena[TensorArenaSize];
 
+// Led to indicate the resulted values
+PwmOut led(LED1);
 
 void setLed(float value) {
   led = abs(value);
 }
 
-// Output the results.
-// Log the current X and Y values
-
 void printValues(float x, float y) {
   printf("x_value: % 2.3f, y_value: % 02.3f\n", x, y);
 }
 
-float infeer(float x) {
-  // Place our calculated x value in the model's input tensor
+float inference(float x) {
   input->data.f[0] = x;
-
-  // Run inference, and report any error
   interpreter->Invoke();
-
-  // Read the predicted y value from the model's output tensor
   return output->data.f[0];
 }
 
-
-
+float generateNextPosition() {
+    float position = (float) inference_count / InferencesPerCycle;
+    float y_value =  position * xrange;
+    inference_count = (inference_count + 1) % InferencesPerCycle;
+    return y_value;
+}
 
 int main(void) {
-  
-  // Set up logging. Google style is to avoid globals or statics because of
-  // lifetime uncertainty, but since this has a trivial destructor it's okay.
-  // NOLINTNEXTLINE(runtime-global-variables)
-  // static MicroErrorReporter micro_error_reporter;
-  // error_reporter = &micro_error_reporter;
 
-  // Map the model into a usable data structure. This doesn't involve any
-  // copying or parsing, it's a very lightweight operation.
-  model = GetModel(g_model);
-
-  // This pulls in all the operation implementations we need.
-  // NOLINTNEXTLINE(runtime-global-variables)
   static AllOpsResolver resolver;
-
-  // Build an interpreter to run the model with.
-  static MicroInterpreter static_interpreter(
-      model, resolver, tensor_arena, TensorArenaSize, NULL);
+  static MicroInterpreter static_interpreter(model, resolver, tensor_arena, TensorArenaSize, NULL);
   interpreter = &static_interpreter;
-
-  // Allocate memory from the tensor_arena for the model's tensors.
   interpreter->AllocateTensors();
 
-
-  // Obtain pointers to the model's input and output tensors.
   input = interpreter->input(0);
   output = interpreter->output(0);
 
   while (true) {
-    // Calculate an x value to feed into the model. We compare the current
-    // inference_count to the number of inferences per cycle to determine
-    // our position within the range of possible x values the model was
-    // trained on, and use this to calculate a value.
-    float position = (float) inference_count / InferencesPerCycle;
-    float x_val = position * kXrange;
+    float x_value = generateNextPosition();
 
-    float y_val = infeer(x_val);
+    float y_value = inference(x_value);
 
-    setLed(y_val);
-    printValues(x_val, y_val);
-
-    // Increment the inference_counter, and reset it if we have reached
-    // the total number per cycle
-    inference_count = (inference_count + 1) % InferencesPerCycle;
-    // if (inference_count >= InferencesPerCycle) inference_count = 0;
+    setLed(y_value);
+    printValues(x_value, y_value);
   }
 }
 
