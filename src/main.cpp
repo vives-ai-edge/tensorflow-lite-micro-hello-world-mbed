a569b8f4ae6d7db20adc71ed5cb2434cef979b6a
Sille Van Landschoot <info@sillevl.be>
2021-12-01 16:27:45 +0100

    update to latest tflm library

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@ limitations under the License.
 #include "tensorflow/lite/micro/micro_error_reporter.h"
 #include "tensorflow/lite/micro/micro_interpreter.h"
 #include "tensorflow/lite/schema/schema_generated.h"
-#include "tensorflow/lite/version.h"
+// #include "tensorflow/lite/version.h"
 
 #include "model.h"
 
